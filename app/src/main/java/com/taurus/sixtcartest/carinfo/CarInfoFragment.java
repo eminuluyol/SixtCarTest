7b6db8ff69ff4ef41632f1467218ddbf6749da2a
Mehmet Emin Uluyol <eminuluyol@Mehmets-MacBook-Pro.local>
2017-07-27 17:36:55 +0300

    Add car info layout

--- app/src/main/java/com/taurus/sixtcartest/carinfo/CarInfoFragment.java
+++ app/src/main/java/com/taurus/sixtcartest/carinfo/CarInfoFragment.java
@@ -1,5 +1,10 @@
 package com.taurus.sixtcartest.carinfo;
 
+import android.support.v4.widget.NestedScrollView;
+import android.support.v7.widget.RecyclerView;
+import android.widget.ImageButton;
+import butterknife.BindView;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
 import com.taurus.sixtcartest.R;
 import com.taurus.sixtcartest.core.BaseFragment;
 
@@ -12,6 +17,18 @@ public class CarInfoFragment extends BaseFragment<CarInfoView, CarInfoPresenter>
   public static CarInfoFragment newInstance() {
     return new CarInfoFragment();
   }
+
+  @BindView(R.id.carInfoRecyclerView)
+  RecyclerView carInfoRecyclerView;
+
+  @BindView(R.id.emptyView)
+  NestedScrollView emptyView;
+
+  @BindView(R.id.carInfoButtonOpenMap)
+  ImageButton buttonOpenMap;
+
+  @BindView(R.id.sliding_layout)
+  SlidingUpPanelLayout slidingLayout;
 
   @Override
   protected int getLayoutResId() {
